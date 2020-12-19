00df1004559b8d43e225c609073580282264164e
kjoos22 <kjoos22@gmail.com>
2020-12-19 16:41:03 -0500

    projects table migration

--- db/migrate/20201219211715_create_projects.rb
+++ db/migrate/20201219211715_create_projects.rb
@@ -1,4 +1,12 @@
 class CreateProjects < ActiveRecord::Migration
   def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state_abbrv
+      t.string :zip
+      t.string :no_site
+    end
   end
 end
