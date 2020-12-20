21a7352463a3201de9cf0a0ba8b4cb164af16aae
kjoos22 <kjoos22@gmail.com>
2020-12-20 17:07:59 -0500

    added projects show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,11 +6,15 @@ class ProjectsController < ApplicationController
         erb :'/projects/index'
     end
 
+    #create
+    get 'projects/new' do
+        erb :'projects/new'
+    end
+
     #show
     get '/projects/:id' do
         @p = Project.find_by(id: params[:id])
         erb :'/projects/show'
-    end
+    end    
     
-    #create
 end
