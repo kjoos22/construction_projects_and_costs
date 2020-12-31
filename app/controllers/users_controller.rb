e0f62c1bd30fbc44476af6e4fd063c2ceaa78474
kjoos22 <kjoos22@gmail.com>
2020-12-31 13:29:50 -0500

    added login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,24 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/login' do
+        erb :'/users/login'
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/projects'
+        else
+            @errors = "Invalid Credentials"
+            erb :'/users/login'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to '/login'
+    end   
 
 end
